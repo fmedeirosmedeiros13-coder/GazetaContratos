496f0407c107196264fc95a4f3d467965566d1de
fmedeirosmedeiros13-coder <fmedeirosmedeiros13@gmail.com>
2026-07-26 15:32:03 -0300

    deploy sw.js (GazetaContratos v1.4)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* GazetaContratos — service worker */
-const CACHE = "gazeta-contratos-v1.3";
+const CACHE = "gazeta-contratos-v1.4";
 const SHELL = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
